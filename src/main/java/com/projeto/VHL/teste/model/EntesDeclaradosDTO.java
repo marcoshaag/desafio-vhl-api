01bacfae37112c10a3cf1942c8f2e37dbab17db3
marcoshaag <marcoshaag2019@gmail.com>
2025-06-02 20:40:59 -0300

    feat: cria EntesDeclaradosDTO

---
+++ src/main/java/com/projeto/VHL/teste/model/EntesDeclaradosDTO.java
@@ -0,0 +1,16 @@
+package com.projeto.VHL.teste.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EntesDeclaradosDTO {
+
+    private Long codigo;
+    private String lei;
+    private String nomeEntidade;
+}
